d13b210eb107353a51e27dd8f8155c4167ab0e22
Christos Papageorgiou <root.expert.xda@gmail.com>
2021-04-23 16:07:27 +0300

    Make Broker extend Node interface
    Signed-off-by: Christos Papageorgiou <root.expert.xda@gmail.com>

--- src/main/java/org/aueb/ds/pubsub/Broker.java
+++ src/main/java/org/aueb/ds/pubsub/Broker.java
@@ -1,9 +1,12 @@
 package org.aueb.ds.pubsub;
 
+import org.aueb.ds.model.Connection;
+import org.aueb.ds.model.Node;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Broker implements Serializable {
+public class Broker implements Node, Serializable {
 
     private ArrayList<Consumer> registeredUsers = new ArrayList<>();
     private ArrayList<Publisher> registeredPublishers = new ArrayList<>();
@@ -37,6 +40,31 @@ public class Broker implements Serializable {
     }
 
     public void filterConsumers(String consumer) {
+
+    }
+
+    @Override
+    public void init(String ip, int port) {
+
+    }
+
+    @Override
+    public ArrayList<Broker> getBrokers() {
+        return null;
+    }
+
+    @Override
+    public Connection connect(String ip, int port) {
+        return null;
+    }
+
+    @Override
+    public void disconnect(Connection connection) {
+
+    }
+
+    @Override
+    public void updateNodes() {
 
     }
 }
