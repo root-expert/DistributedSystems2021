6ca83d6ab1014d19400a610cabeea68950fcfa1a
Christos Papageorgiou <root.expert.xda@gmail.com>
2021-05-08 10:46:37 +0300

    Implement connection between Brokers
    Signed-off-by: Christos Papageorgiou <root.expert.xda@gmail.com>

--- src/main/java/org/aueb/ds/model/Node.java
+++ src/main/java/org/aueb/ds/model/Node.java
@@ -2,6 +2,7 @@ package org.aueb.ds.model;
 
 import org.aueb.ds.pubsub.Broker;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public interface Node {
@@ -10,7 +11,7 @@ public interface Node {
 
     ArrayList<Broker> getBrokers();
 
-    Connection connect(String ip, int port);
+    Connection connect(String ip, int port) throws IOException;
 
     void disconnect(Connection connection);
 
